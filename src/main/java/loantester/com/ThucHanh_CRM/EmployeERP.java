f0819ad694062a028a1abc1b3a98655cfd143914
DESKTOP-VC87G8L\DEV <mailoan201296gmail.com>
2022-11-17 18:59:15 +0700

    Thêm TaskERP

---
+++ src/main/java/loantester/com/ThucHanh_CRM/EmployeERP.java
@@ -0,0 +1,50 @@
+package loantester.com.ThucHanh_CRM;
+
+import loantester.com.Common.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+public class EmployeERP extends BaseTest {
+
+    public static void loginERP(){
+        driver.get("https://app.hrsale.com/erp/login");
+        driver.findElement(By.xpath("//input[@id='iusername']")).sendKeys("frances.burns");
+        driver.findElement(By.xpath("//input[@id='ipassword']")).sendKeys("frances.burns");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+    }
+
+    public static void oppenEmployeePage(){
+        driver.findElement(By.xpath("//span[normalize-space()='Employees']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Add New']")).click();
+        driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys("lƯƠNG ");
+        driver.findElement(By.xpath("//input[@name='last_name']")).sendKeys("BÍCH HỮU");
+        driver.findElement(By.xpath("//input[@name='employee_id']")).clear();
+        driver.findElement(By.xpath("//input[@name='employee_id']")).sendKeys("012345678");
+        driver.findElement(By.xpath("//input[@name='contact_number']")).sendKeys("888888");
+        driver.findElement(By.xpath("//label[normalize-space()='Gender']/following-sibling::span[normalize-space()='Male']")).click();
+        driver.findElement(By.xpath("//span[@class='select2-search select2-search--dropdown']/input")).sendKeys("Female", Keys.ENTER);
+        driver.findElement(By.xpath("//input[contains(@name,'email')]")).sendKeys("minh1234@gmail.com");
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("admin");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("888888");
+        driver.findElement(By.xpath("//label[@class='control-label']/following-sibling::span[normalize-space()='Office Shift']")).click();
+        driver.findElement(By.xpath("//span[@class='select2-search select2-search--dropdown']//input")).sendKeys("Morning Shift",Keys.ENTER);
+    //    driver.findElement(By.xpath(""));
+    //    driver.findElement(By.xpath(""));
+
+
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        creatDriver();
+        loginERP();
+        oppenEmployeePage();
+
+        closeDriver();
+    }
+}
--- src/main/java/loantester/com/ThucHanh_CRM/TaskERP.java
+++ src/main/java/loantester/com/ThucHanh_CRM/TaskERP.java
@@ -2,35 +2,42 @@ package loantester.com.ThucHanh_CRM;
 
 import loantester.com.Common.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 public class TaskERP extends BaseTest {
 
     //login ERP
-    public static void loginERP(){
+    public static void loginERP() {
         driver.get("https://app.hrsale.com/erp/login");
-    //    driver.findElement(By.xpath("//input[@id='iusername']")).clear();
-    //    driver.findElement(By.xpath("//input[@id='ipassword']")).clear();
-    //    sleep(1);
+        //    driver.findElement(By.xpath("//input[@id='iusername']")).clear();
+        //    driver.findElement(By.xpath("//input[@id='ipassword']")).clear();
+        //    sleep(1);
         driver.findElement(By.xpath("//input[@id='iusername']")).sendKeys("frances.burns");
         driver.findElement(By.xpath("//input[@id='ipassword']")).sendKeys("frances.burns");
         driver.findElement(By.xpath("//span[normalize-space()='Login']")).click();
     }
 
-    public static void oppenTaskPage(){
+    public static void oppenTaskPage() {
         driver.findElement(By.xpath("//span[normalize-space()='Tasks']")).click();
         driver.findElement(By.xpath("//a[normalize-space()='Add Task']")).click();
 
     }
 
-    public static void enterDataAddTask(){
+    public static void enterDataAddTask() {
         driver.findElement(By.xpath("//input[@name='task_name']")).sendKeys("KhoiMy");
         driver.findElement(By.xpath("//div[@class='input-group']//input[@name='start_date']")).click();
         driver.findElement(By.xpath("//a[contains(normalize-space(),'17')]")).click();
         driver.findElement(By.xpath("(//button[@class='dtp-btn-ok btn btn-flat btn-primary btn-sm'])[1]")).click();
-       driver.findElement(By.xpath("//input[@name='end_date']")).click();
-      driver.findElement(By.xpath("(//a[contains(normalize-space(),'25')])[2]")).click();
+        driver.findElement(By.xpath("//input[@name='end_date']")).click();
+        driver.findElement(By.xpath("(//a[contains(normalize-space(),'25')])[2]")).click();
         driver.findElement(By.xpath("(//button[@class='dtp-btn-ok btn btn-flat btn-primary btn-sm'])[2]")).click();
-        sleep(1);
+        driver.findElement(By.xpath("//input[@name='task_hour']")).sendKeys("21");
+        driver.findElement(By.xpath("//span[@class='select2-selection select2-selection--single']")).click();
+        driver.findElement(By.xpath("//span[@class='select2-search select2-search--dropdown']/input")).sendKeys("Advertising Platform AD.", Keys.ENTER);
+        driver.findElement(By.xpath("//textarea[@id='summary']")).sendKeys("Theo quyết định số 3730, Bộ Giáo dục và Đào tạo (GD-ĐT) phê duyệt liên kết tổ chức thi cấp chứng chỉ tiếng Anhgiữa Công ty TNHH Giáo dục IDP (Việt Nam) và IELTS Australia Pty Ltd (Úc).");
+
+        driver.findElement(By.xpath("//button[normalize-space()='Save']")).click();
+
 
 
         //    driver.findElement(By.xpath(""));
